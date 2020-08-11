7236c8305d3b043b24d421c95e23cf418699d82e
kazerine <kamikaze.mrinal@gmail.com>
2020-08-12 00:18:36 +0530

    davinci: Get rid of megvii dependencies in camera.qcom.so
    Change-Id: I97058c320d06819a468ebe38e2704360e9aa91db
    Signed-off-by: Arian <arian.kulmer@web.de>

--- device.mk
+++ device.mk
@@ -112,6 +112,9 @@ PRODUCT_PACKAGES += \
     libdng_sdk.vendor \
     GoogleCamera
 
+PRODUCT_PACKAGES += \
+    libshim_camera
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
