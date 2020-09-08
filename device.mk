32d0aced37a29636bb8b8d69e408b2c5ca59e572
Arian <arian.kulmer@web.de>
2020-09-08 16:48:34 +0200

    davinci: Remove QTI Power HAL
    Revert "davinci: power: Scan input devices for eligible DT2W node"
    
    This reverts commit a4c9f8201b973e9f2b27885006b4fec9296c5b22.
    
    Revert "davinci: power: Support event-based dt2w node"
    
    This reverts commit a0e115027a83be763acf55d4514280323eac506a.
    
    Revert "davinci: power: Prepare HAL extension"
    
    This reverts commit b2d6a22c11df44a6de93f36d34874390356448ba.
    
    Revert "davinci: power: Build HAL"
    
    This reverts commit d884716edc4f399c581b8bcb8a813e747d4ecb27.

--- device.mk
+++ device.mk
@@ -279,10 +279,6 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-statix
 
-# Power
-PRODUCT_PACKAGES += \
-    android.hardware.power@1.2-service-qti
-
 # Product characteristics
 PRODUCT_CHARACTERISTICS := nosdcard
 
