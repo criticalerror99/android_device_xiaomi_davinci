6c4ea2586a6b9ba5b08a2e1b74e75656c01c7328
AdarshGrewal <adarshgrewal@gmail.com>
2020-09-25 18:02:02 +0530

    davinci: init: set fingerprint from sunfish

--- init/init_davinci.cpp
+++ init/init_davinci.cpp
@@ -111,7 +111,7 @@ void vendor_load_properties() {
 
     // SafetyNet workaround
     property_override("ro.boot.verifiedbootstate", "green");
-    fingerprint = "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys";
+    fingerprint = "google/sunfish/sunfish:11/RP1A.200720.011/6746289:user/release-keys";
 
     set_ro_build_prop("fingerprint", fingerprint);
     set_ro_product_prop("device", device);
