4d5f59b654a9b6572010cc75291efca0cf9af5fa
criticalerror99 <k99@gmx.com>
2020-10-05 20:43:21 +0200

    exui-fy

--- exui_davinci.mk
+++ exui_davinci.mk
@@ -14,10 +14,21 @@
 # limitations under the License.
 #
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/exui_davinci.mk
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-COMMON_LUNCH_CHOICES := \
-    exui_davinci-user \
-    exui_davinci-userdebug \
-    exui_davinci-eng
+# Inherit from davinci device
+$(call inherit-product, device/xiaomi/davinci/device.mk)
+
+# Inherit some common ExtendedUI stuff.
+$(call inherit-product, vendor/exui/config/common.mk)
+TARGET_USES_BLUR := true
+
+PRODUCT_NAME := exui_davinci
+PRODUCT_DEVICE := davinci
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Mi 9T
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
